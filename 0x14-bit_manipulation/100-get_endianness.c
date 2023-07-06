51d77713940a9e3d548e379ce9c915585ff18044
hassna idelarbi <idelarbihassna97@gmail.com>
2023-07-06 16:09:38 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - returns the endianness of the system
+ * Return: 0 for big endian, 1 if small
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+
+	return (*(char *)&n);
+
+}
