4b23fcf6f6afd8574188c5692c9c3125bbd82840
hassna idelarbi <idelarbihassna97@gmail.com>
2023-07-11 06:41:29 -0700

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file and prints it
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: number bytes read/printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+return (0);
+	fd = open(filename, 0_RDNLY);
+	if (fd == -1)
+	return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (bytes);
+}
